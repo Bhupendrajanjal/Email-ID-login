1d6cec09247d5da92e8588949629fd30593cab60
Bhupendra janjal <134838669+Bhupendrajanjal@users.noreply.github.com>
2023-08-03 10:21:11 +0530

    Email id login

---
+++ assets/js/app.js
@@ -0,0 +1,71 @@
+var cl = console.log;
+
+let db=[
+    {
+        email:"bhupendrajanjal7788@gmailcom",
+        Passworld:"123456",
+    }
+]
+
+const loginform = document.getElementById('formgroup');
+const emailcontrol = document.getElementById("email")
+const Passworldcontrol = document.getElementById("Passworld");
+
+const onlogin = (eve)=>{
+    eve.preventDefault();
+    cl("submitted")
+    eve.target.reset();
+    setTimeout(()=>{
+        let emailval = emailcontrol.value;
+        let Passworldval = Passworldcontrol.value;
+       
+    let isemailexist ;
+    db.forEach(acc=>{
+        if(emailval===acc.email){
+            isemailexist=acc;
+          
+        }
+    })
+ 
+    cl(isemailexist)
+    if(isemailexist){
+        //chech passworld
+        if(isemailexist.Passworld=== Passworldval){
+            cl("submitted")
+           redirectToDashboard()
+          
+              
+        }else{
+            Swal.fire({
+                imageUrl: "https://previews.123rf.com/images/tigatelu/tigatelu1411/tigatelu141100038/33887243-sad-smiley-emoticon-cartoon.jpg"
+    ,           imageHeight: 200,
+                imageWeight:400,
+                title:"Invalid Email And Passworld !!!!",
+                imageAlt: 'A tall image'
+              },3000)
+        }
+
+
+    }
+    else{
+        Swal.fire({
+            imageUrl: "https://previews.123rf.com/images/tigatelu/tigatelu1411/tigatelu141100038/33887243-sad-smiley-emoticon-cartoon.jpg"
+,           imageHeight: 200,
+            imageWeight:400,
+            title:"Invalid Email Passworld !!!",
+            imageAlt: 'A tall image'
+          },3000)
+    }
+
+
+    },2000)
+}
+
+
+function redirectToDashboard(){
+   window.location.href='dashedboared2.html'
+  
+
+}
+
+loginform .addEventListener("submit",onlogin)
